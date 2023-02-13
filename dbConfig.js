a925ec888fb1a0f3bd74d2751e2ae5752683c109
johnkennady220 <115686295+johnkennady220@users.noreply.github.com>
2023-02-13 22:52:20 +0530

    env

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const dbName = 'webscrapping'
-const dbURL = `mongodb+srv://kandy:TWzOXXvb108QoD4E@cluster0.dw17smr.mongodb.net/?retryWrites=true&w=majority${dbName}`;
+const dbURL = `mongodb+srv://johnkennady220:jADZAvTf2KX4lr0J@cluster0.afdpskn.mongodb.net/?retryWrites=true&w=majority${dbName}`;
 
 module.exports={dbURL,mongodb,MongoClient,dbName}
